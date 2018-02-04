486da12adbb77dca37d9a47bc2a96414ba84100e
i_p_addr@yahoo.com <i_p_addr@yahoo.com>
2018-02-04 11:11:15 +0700

    answer all the three queries

--- main.py
+++ main.py
@@ -3,21 +3,35 @@ import psycopg2
 
 query1 = "select title, count(*) from log join articles on log.path like concat('%', articles.slug, '%') where status = '200 OK' group by title order by count desc;"
 query2 = "select name, count(*) from articles join authors on authors.id = articles.author join log on log.path like concat('%', articles.slug, '%') where log.status = '200 OK' group by authors.name order by count desc;"
-query3 = ""
+query3 = "select date_trunc('day', time), count (*), (select count(*) from log where status != '200 OK')," \
+         "100.0 * count (*) / (select count(*) from log where status != '200 OK') ::float from log where status != '200 OK' group by 1 order by 2 desc;"
 
 try:
     db = psycopg2.connect("dbname=news")
     cur = db.cursor()
 
-    # 1. What are the most popular three articles of all time?
+    # query 1
+    cur.execute(query1)
+    rows = cur.fetchall()
+    for row in rows:
+        print(str(row[0]) + ' , ' + str(row[1]) + ' views')
+
+    print('\n\n\n')
+
+    # query 2
     cur.execute(query2)
     rows = cur.fetchall()
     for row in rows:
         print(str(row[0]) + ' , ' + str(row[1]) + ' views')
 
-    # 2. Who are the most popular article authors of all time?
-    # cur.execute("select name, count(name) from authors right join articles on authors.id = articles.author group by name order by count desc;")
+    print('\n\n\n')
 
-except:
+    # query 1
+    cur.execute(query3)
+    rows = cur.fetchall()
+    for row in rows:
+        print(str(row[0]) + ' , ' + str(row[3]) + '% errors')
+
+except Exception as e:
     print "I am unable to connect to the database"
-
+    print e
--- solution.sql
+++ solution.sql
@@ -1,3 +1,3 @@
-Question 1 : select path, count(*) from log where status = '200 OK' group by path order by count desc;
+Question 1 : select title, count(*) from log join articles on log.path like concat('%', articles.slug, '%') where status = '200 OK' group by title order by count desc;
 Question 2 : select name, count(*) from articles join authors on authors.id = articles.author join log on log.path like concat('%', articles.slug, '%') where log.status = '200 OK' group by authors.name order by count desc;
-Question 3 :
+Question 3 : SELECT date_trunc('day', time) as “date” , count(*) as 'total' from log where status != '200 OK' group by 1 order by count desc
