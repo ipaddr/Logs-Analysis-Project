74e1eee8589c679195490c8498e9cfa94cae5ee7
Iip Permana <i_p_addr@yahoo.com>
2018-02-08 23:51:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # Reference from https://wiki.postgresql.org/wiki/Psycopg2_Tutorial
 import psycopg2
 
-query1 = "select title, count(*) from log join articles on log.path like concat('%', articles.slug, '%') where status = '200 OK' group by title order by count desc;"
-query2 = "select name, count(*) from articles join authors on authors.id = articles.author join log on log.path like concat('%', articles.slug, '%') where log.status = '200 OK' group by authors.name order by count desc;"
-query3 = "select date_trunc('day', time), count (*), (select count(*) from log where status != '200 OK')," \
-         "100.0 * count (*) / (select count(*) from log where status != '200 OK') ::float from log where status != '200 OK' group by 1 order by 2 desc;"
+query1 = "select title, count(*) from log join articles on " \
+         " log.path like concat('%', articles.slug, '%') where " \
+         " status = '200 OK' group by title order by count desc;"
+query2 = " select name, count(*) from articles join authors on " \
+         " authors.id = articles.author join log on log.path " \
+         " like concat('%', articles.slug, '%') where " \
+         " log.status = '200 OK' group by authors.name order by count desc;"
+query3 = " select date_trunc('day', time), count (*), " \
+         " (select count(*) from log where status != '200 OK')," \
+         " 100.0 * count (*) / (select count(*) from log where " \
+         " status != '200 OK') ::float from log where " \
+         " status != '200 OK' group by 1 order by 2 desc;"
 
 try:
     db = psycopg2.connect("dbname=news")
@@ -34,4 +42,3 @@ try:
 
 except Exception as e:
     print "I am unable to connect to the database"
-    print e
