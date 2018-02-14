b27cc098621ef7e77d9253f75f647bf302904869
Iip Permana <i_p_addr@yahoo.com>
2018-02-14 13:41:24 +0700

    Delete solution.sql
    no need this file

--- solution.sql
+++
@@ -1,3 +0,0 @@
-Question 1 : select title, count(*) from log join articles on log.path like concat('%', articles.slug, '%') where status = '200 OK' group by title order by count desc;
-Question 2 : select name, count(*) from articles join authors on authors.id = articles.author join log on log.path like concat('%', articles.slug, '%') where log.status = '200 OK' group by authors.name order by count desc;
-Question 3 : SELECT date_trunc('day', time) as “date” , count(*) as 'total' from log where status != '200 OK' group by 1 order by count desc
